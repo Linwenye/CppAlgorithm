68f5fb778c5586e0d823ec83e9422b994c5d4054
LinWenye <419766878@qq.com>
2020-01-01 18:01:59 +0800

    Initial Commit

---
+++ CppAlgorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppAlgorithm
+//
+//  Created by 林文烨 on 2020/1/1.
+//  Copyright © 2020 林文烨. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
