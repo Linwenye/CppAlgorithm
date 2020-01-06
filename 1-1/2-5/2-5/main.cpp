0d6d84985db3803ffdb31036290326330f5db552
LinWenye <419766878@qq.com>
2020-01-06 20:37:40 +0800

    2-5

---
+++ 1-1/2-5/2-5/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  2-5
+//
+//  Created by 林文烨 on 2020/1/6.
+//  Copyright © 2020 林文烨. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+int main(int argc, const char *argv[])
+{
+    // insert code here...
+    int a, b, c;
+    int case_i = 1;
+    while (scanf("%d %d %d", &a, &b, &c) != EOF) {
+        if (a == 0 && b == 0 && c == 0) {
+            break;
+        }
+        string print_str = string("Case %d: %.")+to_string(c)+string("lf\n");
+//        Case %d: %.if
+        printf((char *)print_str.c_str(), case_i++, double(a) / double(b));
+    }
+    return 0;
+}
