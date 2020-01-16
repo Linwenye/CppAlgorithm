cf55e2276774fe6a78990a871e15663637b231a5
LinWenye <419766878@qq.com>
2020-01-16 17:39:11 +0800

    3-11

--- 3-11/3-11/main.cpp
+++ 3-11/3-11/main.cpp
@@ -7,9 +7,39 @@
 //
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char *argv[])
+{
+    // brute force
+    string up, down;
+    while (cin >> up)
+    {
+        cin >> down;
+        if (up.length() > down.length())
+            swap(up, down);
+
+        int res = int(up.length() + down.length());
+        for (int i = -int(up.length()) + 1; i < int(down.length()); i++)
+        {
+            bool match = true;
+            for (int down_k = max(0, i), up_k = max(0, -i); down_k < min(int(down.length()), i + int(up.length())); down_k++, up_k++)
+            {
+                if (down[down_k] == '2' && up[up_k] == '2')
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if (match)
+            {
+                int new_length = int(up.length()) + int(down.length()) - (min(int(down.length()), i + int(up.length())) - max(0, i));
+                res = new_length < res ? new_length : res;
+            }
+        }
+        cout << res << endl;
+    }
     return 0;
 }
